84589ba03943f64043091bf86158a940d4775907
Rakul <rabrosrajgopu@gmail.com>
2023-01-26 09:01:17 +0100

    added firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // Firbase Verbindungsparameter hinzufügen
+  firebaseConfig: {
+    apiKey: "AIzaSyArTTnqykFNgwBRL8IldPCFn8dobVVghBI",
+    authDomain: "m335-login.firebaseapp.com",
+    databaseURL: "https://m335-login.firebaseio.com",
+    projectId: "m335-login",
+    storageBucket: "m335-login.appspot.com",
+    messagingSenderId: "14955115856",
+    appId: "1:14955115856:web:d53930b7c82b6d01106855"
+  }
 };
 
 /*
